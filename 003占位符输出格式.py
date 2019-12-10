b41fe6b2d82efc74df9299318c92b32e2df0944e
kaikai <andangel@vip.qq.com>
2019-12-10 19:17:20 +0800

    Default

---
+++ 003占位符输出格式.py
@@ -0,0 +1,9 @@
+addressee='黄程凯'
+
+receivingAddress='厦门市'
+
+contactNumber='18950055005'
+
+quantityArticles= '5'
+
+print('订单收件人:%s,收货地址:%s,联系方式:%s,商品数量:%s'%(addressee,receivingAddress,contactNumber,quantityArticles))
